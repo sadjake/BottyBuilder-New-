b4e8338ef921a65ac74e2c3b395862fcea2bc0e9
Sophia <66497192+sophiadt@users.noreply.github.com>
2023-08-21 11:07:52 -0700

    ya

---
+++ main.py
@@ -0,0 +1,9 @@
+from SECRET import token
+import nextcord, time
+from dotenv import load_dotenv
+from nextcord import Interaction
+from nextcord.ext import commands
+
+import logging
+import os
+from typing import Optional
